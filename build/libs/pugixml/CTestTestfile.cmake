7695aa3ad61dce3d6d5618174f84f06b74b74817
Kares <CarlosEscribano113@gmail.com>
2026-09-25 14:56:13 +0200

    Gitignore modified

--- build/libs/pugixml/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/carlo/Desktop/Master/SegundoSemestre/RA2/RenderingTechniques/libs/pugixml
-# Build directory: C:/Users/carlo/Desktop/Master/SegundoSemestre/RA2/RenderingTechniques/build/libs/pugixml
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
